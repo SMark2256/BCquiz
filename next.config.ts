5ac2c76ba548a01dd8022f03c2b883ea21747669
scridon-siklodi.mark <scridon-siklodi.mark@trium-itech.hu>
2026-06-15 17:11:10 +0200

    fix: unoptimized added to config

--- next.config.ts
+++ next.config.ts
@@ -14,15 +14,16 @@ const nextConfig: NextConfig = {
     ],
   },
   images: {
-    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**', // Engedélyez minden aldomaint és fődomaint
+        protocol: "https",
+        hostname: "**", // Engedélyez minden aldomaint és fődomaint
       },
       {
-        protocol: 'http',
-        hostname: '**', // Engedélyez minden nem biztonságos forrást is, ha szükséges
+        protocol: "http",
+        hostname: "**", // Engedélyez minden nem biztonságos forrást is, ha szükséges
       },
     ],
   },
